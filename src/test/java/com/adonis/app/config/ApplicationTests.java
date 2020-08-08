168d37f0f9d869bfbeb44837f6bd3e72528c7e7f
Vasanthan Ravindran <vasanth.ravindran@gmail.com>
2020-08-08 08:20:54 -0400

    Fully functional Config Server

---
+++ src/test/java/com/adonis/app/config/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adonis.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
